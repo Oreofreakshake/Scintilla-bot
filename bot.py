baf5ba03df8e1944cb1d85e84c879491eddfdb49
Oreofreakshake <yoosufyazak@gmail.com>
2021-10-10 23:41:03 +0500

    added the help command

--- bot.py
+++ bot.py
@@ -9,9 +9,27 @@ TOKEN = os.getenv("TOKEN")
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 
-@bot.message_handler(commands=["start", "help"])
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
-    bot.reply_to(message, "saam is gay, i'm just testing the API")
+    textList = [
+        "Hello i'm online, type /help to get the bot commands, I'll add the saam commands later"
+    ]
+    bot.send_message(text=textList[0], chat_id=-1001467819274)
+
+
+@bot.message_handler(commands=["help"])
+def send_welcome(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.add(
+        telebot.types.InlineKeyboardButton(
+            "Message the developer", url="telegram.me/zuccccc"
+        )
+    )
+    bot.send_message(
+        chat_id=-1001467819274,
+        text="- /start to check if the bot is online\n- /('anything you want') to echo whatever you say\n",
+        reply_markup=keyboard,
+    )
 
 
 @bot.message_handler(func=lambda m: True)
