9c60f96af565932a32bfda0a504b5a5d05d695c0
Oreofreakshake <53213763+Oreofreakshake@users.noreply.github.com>
2021-11-19 22:33:31 +0500

    Set up

---
+++ bot.py
@@ -0,0 +1,30 @@
+import os
+import telebot
+from dotenv import load_dotenv
+import random
+
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=["start"])
+def welcome_text(message):
+    bot.reply_to(message, "Hello, I'm working just fine!")
+
+
+def saam_meter(message):
+    tempINT = random.choice(range(101))
+    value = str(tempINT)
+
+    if tempINT > 90:
+        bot.reply_to(message, "You are " + value + "% saam, damn unlucky")
+    else:
+        bot.reply_to(message, "You are " + value + "% saam")
+
+
+if __name__ == "__main__":
+    bot.infinity_polling()
