601661da209010e0e9838154fb7a3e3a7aa05c20
Oreofreakshake <53213763+Oreofreakshake@users.noreply.github.com>
2022-01-17 06:17:34 +0500

    hacker man

--- bot.py
+++ bot.py
@@ -1,19 +1,25 @@
 import os
 import telebot
 from dotenv import load_dotenv
-import logging
+from cogs import commands
+
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 
 bot = telebot.TeleBot(TOKEN)
-logging.basicConfig(level=logging.INFO)
 
 
 @bot.message_handler(commands=["start"])
 def welcome_text(message):
     bot.reply_to(message, "Hello, I'm working just fine!")
+
+
+@bot.message_handler(commands=["saamometer"])
+def command_one(message):
+    value = commands.Commands(bot)
+    value.saam_meter(message)
 
 
 if __name__ == "__main__":
--- cogs/commands.py
+++ cogs/commands.py
@@ -1,13 +1,14 @@
 import random
 
 
+class Commands:
+    def __init__(self, bot):
+        self.bot = bot
 
-#@bot.message_handler(commands=["saamometer"])
-#def saam_meter(message):
-#    tempINT = random.choice(range(101))
-#    value = str(tempINT)
-#
-#    if tempINT > 90:
-#        bot.reply_to(message, "You are " + value + "% saam, damn unlucky")
-#    else:
-#        bot.reply_to(message, "You are " + value + "% saam")
+    def saam_meter(self, message):
+        tempINT = random.choice(range(101))
+        value = str(tempINT)
+        if tempINT > 90:
+            self.bot.reply_to(message, "You are " + value + "% saam, damn unlucky")
+        else:
+            self.bot.reply_to(message, "You are " + value + "% saam")
