ac86bd05415fa8e23c2b49837e4f000f72cd9ddf
Oreofreakshake <53213763+Oreofreakshake@users.noreply.github.com>
2022-01-18 20:59:09 +0500

    Got all the prayer Data

--- api/prayer.py
+++ api/prayer.py
@@ -8,11 +8,62 @@ session = requests.get(url)
 soup = BeautifulSoup(session.content, "html.parser")
 
 
+# fajar data
+
 lists = soup.find_all(
     "div",
     class_="fajar-tile",
 )
 
 for list in lists:
-    NextPrayerTimeData = list.find("span", class_="prayertime").text
-    NextPrayerTime = NextPrayerTimeData.lower()
+    FajarData = list.find("span", class_="prayertime").text
+    Fajar = FajarData.lower()
+    # print(Fajar)
+
+# dhuhar data
+
+lists = soup.find_all(
+    "div",
+    class_="dhuhar-tile",
+)
+
+for list in lists:
+    DhuharData = list.find("span", class_="prayertime").text
+    Dhuhar = DhuharData.lower()
+    # print(Dhuhar)
+
+# asr data
+
+lists = soup.find_all(
+    "div",
+    class_="asr-tile",
+)
+
+for list in lists:
+    AsrData = list.find("span", class_="prayertime").text
+    Asr = AsrData.lower()
+    # print(Asr)
+
+# maghrib data
+
+lists = soup.find_all(
+    "div",
+    class_="maghrib-tile",
+)
+
+for list in lists:
+    MaghribData = list.find("span", class_="prayertime").text
+    Maghrib = MaghribData.lower()
+    # print(Maghrib)
+
+# isha data
+
+lists = soup.find_all(
+    "div",
+    class_="isha-tile",
+)
+
+for list in lists:
+    IshaData = list.find("span", class_="prayertime").text
+    Isha = IshaData.lower()
+    # print(Isha)
