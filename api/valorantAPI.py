1bb5e9d02f33867f0be014af1aa021ed8ea969cc
Yoosuf Yazak Hafiz <U3105@bml.com.mv>
2023-01-18 17:07:46 +0500

    added valorant command

---
+++ api/valorantAPI.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def GetAccount(username, tag):
+    acc = f"https://api.henrikdev.xyz/valorant/v1/account/{username}/{tag}"
+    json_data_acc = requests.get(acc).json()
+    return json_data_acc
+
+
+def GetRank(username, tag, region):
+    rank = f"https://api.henrikdev.xyz/valorant/v1/mmr/{region}/{username}/{tag}"
+    json_data_rank = requests.get(rank).json()
+    return json_data_rank
+
+
+print("Valorant cogs are ready!\n")
--- cogs/commands/valorant.py
+++ cogs/commands/valorant.py
@@ -1,12 +1,46 @@
+from api.valorantAPI import GetAccount, GetRank
+
+
 def extract_arg(arg):
     data = arg.split("/valorant ")[-1]
     return data
 
 
 async def valorant(bot, message):
-    riotID = extract_arg(message.text)
-    if riotID[0] != "/":
-        print(riotID)
+    riotIDarg = extract_arg(message.text)
+    if riotIDarg[0] != "/":
+        if "#" in riotIDarg:
+            riotID = riotIDarg.split("#")
+            username = riotID[0]
+            tag = riotID[-1]
+
+            if len(tag) >= 2 and len(tag) <= 5:
+                accountData = GetAccount(username, tag)
+                region = accountData["data"]["region"]
+                accountRank = GetRank(username, tag, region)
+
+                rank = accountRank["data"]["currenttierpatched"]
+                user = accountData["data"]["name"] + "#" + accountData["data"]["tag"]
+                level = accountData["data"]["account_level"]
+                userregion = accountData["data"]["region"]
+
+                await bot.send_message(
+                    message.chat.id,
+                    f"riot ID: {user}\nlevel: {level}\nrank: {rank}\nregion: {userregion}",
+                )
+            else:
+                await bot.send_message(
+                    message.chat.id,
+                    f"your tag is invalid as well, try again\n `/valorant`",
+                    parse_mode="MarkdownV2",
+                )
+
+        else:
+            await bot.send_message(
+                message.chat.id,
+                f"I need your tag, try again\n `/valorant`",
+                parse_mode="MarkdownV2",
+            )
     else:
         await bot.send_message(
             message.chat.id,
