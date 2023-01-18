7e65a603ba8b370791f26ec2aa9d50400c095cc7
Yoosuf Yazak Hafiz <U3105@bml.com.mv>
2023-01-18 09:30:55 +0500

    updated archive feature

--- cogs/commands/archive.py
+++ cogs/commands/archive.py
@@ -1,11 +1,33 @@
 async def archive(bot, message):
-        await bot.send_message(
-            message.chat.id,
-            'Reply "save" to archive the message',
-        )
+
+    archiveChatID = -803504766
+
+    if message.chat.type != "private":
+        if message.chat.id == archiveChatID:
+
+            await bot.send_message(message.chat.id, "You can't archive here")
+
+        else:
+
+            await bot.send_message(
+                message.chat.id,
+                'Reply "save" to archive the message',
+            )
+    else:
+        await bot.send_message(message.chat.id, "Not supported in private messages")
+
 
 async def bot_reply_archive(bot, message):
-    if message.text == "save":
-        msgID = message.reply_to_message.message_id
-        await bot.send_message(message.chat.id, "archived")
-    await bot.forward_message(-803504766, message.chat.id, msgID)
+    archiveChatID = -803504766
+    if message.chat.type != "private":
+        if message.chat.id == archiveChatID:
+            await bot.send_message(message.chat.id, "You can't archive here")
+
+        else:
+            if message.text == "save":
+                msgID = message.reply_to_message.message_id
+                await bot.send_message(message.chat.id, "archived")
+            await bot.forward_message(-803504766, message.chat.id, msgID)
+
+    else:
+        await bot.send_message(message.chat.id, "You can't archive private messages")
